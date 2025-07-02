e3353f757467c6d9b9ee2d647adf5203d44f8a59
Lazymoo35 <136227911+Lazymoo35@users.noreply.github.com>
2025-07-02 21:01:48 +0700

    Add math question and word bank

--- question_store.py
+++ question_store.py
@@ -47,6 +47,39 @@ class QuestionStore:
             }
         }
 
+        self.operations = {
+            '+': operator.add,
+            '-': operator.sub,
+            '*': operator.mul,
+            '/': operator.floordiv
+        }
+
+        self.word_question = {
+            "easy": [
+                "apple", "house", "water", "music", "happy", "green", "table", "chair", "light", "clock", \
+                "money", "coffee", "pizza", "school", "phone", "train", "river", "bread", "candy", "cloud", \
+                "paper", "window", "flower", "guitar", "doctor", "family", "yellow", "animal", "mountain", "orange", \
+                "pencil", "friend", "cookie", "banana", "beach", "sleep", "market", "camera", "jacket", "shoes", \
+                "island", "bottle", "mirror", "garden", "cheese", "letter", "engine", "planet", "rocket", "castle"
+            ],
+
+            "medium": [
+                "hospital", "library", "language", "holiday", "traffic", "problem", "animal", "message", "weather", "village", \
+                "airport", "journey", "plastic", "history", "science", "company", "picture", "battery", "visitor", "machine", \
+                "address", "project", "student", "kitchen", "festival", "teacher", "painter", "country", "message", "program", \
+                "manager", "opinion", "natural", "subject", "engineer", "balance", "laundry", "passenger", "forever", "include", \
+                "shelter", "feather", "carrier", "station", "pattern", "travel", "meeting", "factory", "recycle", "content"
+            ],
+
+            "hard": [
+                "approval", "strategy", "solution", "decision", "economy", "argument", "attitude", "benefit", "consumer", "creativity", \
+                "culture", "election", "evidence", "exercise", "freedom", "function", "generation", "industry", "influence", "location", \
+                "material", "movement", "opposite", "position", "positive", "pressure", "reaction", "reduction", "relation", "research", \
+                "resource", "response", "security", "selection", "situation", "society", "standard", "strength", "struggle", "suggestion", \
+                "symbol", "technique", "temperature", "tradition", "transport", "universe", "violence", "warning", "weakness", "witness"
+            ]
+        }
+
     def ask_social_question(self):
     """ A method to ask a social question based on difficulty level. """
         country = random.choice(list(self.social_question[self.difficulty].keys()))
@@ -57,3 +90,23 @@ class QuestionStore:
 
         else:
             print(f"❌ Incorrect. The correct answer is {correct_answer}.")
+    
+    def math_question(self):
+    """ A method to formulate math question based on difficulty level. """
+        if difficulty == "easy"
+            num1 = random.randint(1, 20)
+            num2 = random.randint(1, 10)
+            op_symbol = random.choice(list(self.operations.keys()))
+            op_func = self.operations[op_symbol]
+        if difficulty == "medium":
+            num1 = random.randint(5, 50)
+            num2 = random.randint(5, 20)
+            op_symbol = random.choice(list(self.operations.keys()))
+            op_func = self.operations[op_symbol]
+        if difficulty == "hard":
+            num1 = random.randint(15, 30)
+            num2 = random.randint(15, 20)
+            op_symbol = random.choice(list(self.operations.keys()))
+            op_func = self.operations[op_symbol]
+    
+    def 
